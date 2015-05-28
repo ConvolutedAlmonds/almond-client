b044edbefe7c9eaa5ff4e5a120917be3aee0f20c
Kyle Shockey <kyleshockey1@gmail.com>
2015-05-28 14:40:52 -0700

    add reverse geocode directive, display current location on start view

--- www/js/app.js
+++ www/js/app.js
@@ -68,4 +68,27 @@ angular.module('almond', ['ionic', 'almond.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/start');
+})
+
+.directive('reverseGeocode', function () {
+    return {
+        restrict: 'E',
+        template: '<span></span>',
+        link: function (scope, element, attrs) {
+            var geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
+            geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    if (results[1]) {
+                        element.text(results[1].formatted_address);
+                    } else {
+                        element.text('Location not found');
+                    }
+                } else {
+                    element.text('Geocoder failed due to: ' + status);
+                }
+            });
+        },
+        replace: true
+    }
 });
