aaf7c89d6163c6d052a08515d6bfbe03037d3253
Kyle Shockey <kyleshockey1@gmail.com>
2015-05-28 11:38:43 -0700

    add (working!!!!) map element to map view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,5 +38,27 @@ angular.module('almond.controllers', [])
 })
 
 .controller('MapCtrl', function($scope, $stateParams) {
+  console.log("Yo!")
+  // google.maps.event.addDomListener(window, 'load', function() {
+      var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
   
-});
+      var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+  
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  
+      navigator.geolocation.getCurrentPosition(function(pos) {
+          map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          var myLocation = new google.maps.Marker({
+              position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+              map: map,
+              title: "My Location"
+          });
+      });
+  
+      $scope.map = map;
+  });
+// });
