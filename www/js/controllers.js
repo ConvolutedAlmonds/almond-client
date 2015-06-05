5a6f1825a6a87a42d8f9cce026910a1b861beb11
Ansel Rosenberg <ansel01@gmail.com>
2015-06-05 14:19:25 -0700

    Change uber url

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,7 +22,7 @@ angular.module('almond.controllers', [])
 
   };
 
-  var uberUrl = 'uber://?action=setPickup&pickup[formatted_address]=175%20Rae%20Ave%2C%20San%20Francisco%2C%20CA%2094112';
+  var uberUrl = 'uber://?client_id=YOUR_CLIENT_ID&action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d';
 
   $scope.testUber = function() {
     window.open(uberUrl, 'system');
